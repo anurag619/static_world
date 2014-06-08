32ee728826d70c165145077034f41dc030b51405
Anurag <anurag3rdsep@gmail.com>
2014-06-08 07:50:56 +0530

    subdomain

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Anurag'
 SITENAME = u'Static_World'
-SITEURL = 'http://blog.anuragkr.in'
+SITEURL = 'http://static_world.anuragkr.in'
 
 #Navigation sections and relative URL:
 SECTIONS = [('About', 'howdy.html') ,
